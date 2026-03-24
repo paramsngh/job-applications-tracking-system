b11b4520e408eff0577855a58fb5effc33f611c7
paramsngh <143684229+paramsngh@users.noreply.github.com>
2026-03-24 16:23:31 -0400

    Fix ActiveProfiles import

--- company/src/test/java/com/company/company/CompanyApplicationTests.java
+++ company/src/test/java/com/company/company/CompanyApplicationTests.java
@@ -2,8 +2,10 @@ package com.company.company;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CompanyApplicationTests {
 
 	@Test
