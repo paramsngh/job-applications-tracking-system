8720085070130a135591d9286768598e0564e216
paramsngh <143684229+paramsngh@users.noreply.github.com>
2026-03-25 01:20:12 -0400

    Update CucumberSpringConfig.java

--- company/src/test/java/com/company/company/CucumberSpringConfig.java
+++ company/src/test/java/com/company/company/CucumberSpringConfig.java
@@ -2,8 +2,10 @@ package com.company.company;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")   // ⭐ THIS IS THE FIX
 public class CucumberSpringConfig {
 }
